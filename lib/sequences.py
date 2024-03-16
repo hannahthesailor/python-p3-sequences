49ab5a2303252a485372e2da54648daf54b277ed
Hannah Bradley <hannahthesailor@yahoo.com>
2024-03-16 01:26:54 -0400

    python-p3-sequences.md

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print("[]")
+    elif length == 1:
+        print("[0]")
+    else:
+        a, b = 0, 1
+        fib_sequence = [a]
+        for _ in range(1, length):
+            fib_sequence.append(b)
+            a, b = b, a + b
+        print(fib_sequence)
